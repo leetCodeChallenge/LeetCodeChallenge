30fa8871bd4a83a21495025ba66ef12c7c0f8943
Natty Teshome <nattyteshome30@gmail.com>
2022-05-16 17:53:37 -0500

    majority element solved

--- LeetCodeChallenge/src/BestTimeToBuyAndSell.java
+++ LeetCodeChallenge/src/BestTimeToBuyAndSell.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class BestTimeToBuyAndSell {
     public int sellStock(int[] prices){
         int profit = 0;
+        //Arrays.sort(prices);
+        //System.out.println(Arrays.toString(prices));
         for(int i = 0; i<prices.length-1; i++){
             for (int j = i+1; j<prices.length; j++ ){
                 int maxProfit = prices[j] - prices[i];
